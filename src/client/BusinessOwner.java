ec0070a4a9afaadcd7c66d681b67d1570fa5e03b
Ran Lu <s3583185@student.rmit.edu.au>
2017-03-14 12:13:39 +1100

    add variables for BusinessOwner

---
+++ src/client/BusinessOwner.java
@@ -0,0 +1,10 @@
+package client;
+
+public class BusinessOwner {
+	private String BusinessName;
+	private String BusinessOwnerName;
+	private String address;
+	private String phone;
+	private String username;
+	private String password;
+}
