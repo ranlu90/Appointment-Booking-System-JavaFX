289e3bed0516b32c743dabbdad1a01bba71c5d17
Ran Lu <s3583185@student.rmit.edu.au>
2017-03-14 13:44:46 +1100

    add get & set methods for business owner

--- src/client/BusinessOwner.java
+++ src/client/BusinessOwner.java
@@ -7,4 +7,62 @@ public class BusinessOwner {
 	private String phone;
 	private String username;
 	private String password;
+
+	public BusinessOwner(String BusinessName,String BusinessOwnerName,
+						String address,String phone,String username,String password){
+		this.BusinessName = BusinessName;
+		this.BusinessOwnerName = BusinessOwnerName;
+		this.address = address;
+		this.phone = phone;
+		this.username = username;
+		this.password = password;
+	}
+
+	public String getBusinessName(){
+		return BusinessName;
+	}
+
+	public void setBusinessName(String BusinessName){
+		this.BusinessName = BusinessName;
+	}
+
+	public String getBusinessOwnerName(){
+		return BusinessOwnerName;
+	}
+
+	public void setBusinessOwnerName(String BusinessOwnerName){
+		this.BusinessOwnerName = BusinessOwnerName;
+	}
+
+	public String getaddress(){
+		return address;
+	}
+
+	public void setaddress(String address){
+		this.address = address;
+	}
+
+	public String getphone(){
+		return phone;
+	}
+
+	public void setphone(String phone){
+		this.phone = phone;
+	}
+
+	public String getusername(){
+		return username;
+	}
+
+	public void setusername(String username){
+		this.username = username;
+	}
+
+	public String getpassword(){
+		return password;
+	}
+
+	public void setpassword(String password){
+		this.password = password;
+	}
 }
